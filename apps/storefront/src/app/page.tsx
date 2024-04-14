09fdbcbb03b3ce13de4c9efc3d3bd3d313e9733d
michalrejmisz <michalrejmisz11@gmail.com>
2024-04-14 14:50:12 +0200

    Add initial Storefront and Dashboard applications

--- apps/storefront/src/app/page.tsx
+++ apps/storefront/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
+          Create <span className="text-pink-400">Storefront</span> Turbo
         </h1>
         <AuthShowcase />
 
