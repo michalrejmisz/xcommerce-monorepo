676082c84f7bd5aa1d5231a34e0e492103ce13c8
michalrejmisz <michalrejmisz11@gmail.com>
2024-05-19 13:29:54 +0200

    feat(storefront): Added blank pages for product scaffolding

---
+++ apps/storefront/src/app/(products)/product/page.tsx
@@ -0,0 +1,15 @@
+export default function ProductsPage() {
+  //   const ctx = api.useUtils();
+  //   const { data: productsData } = api.product.getAll.useQuery();
+  //   const { mutate: createProduct } = api.product.create.useMutation({
+  //     onSuccess: () => {
+  //       ctx.product.getAll.invalidate();
+  //     },
+  //   });
+
+  return (
+    <div>
+      <div>Produkt</div>
+    </div>
+  );
+}
